9f7fcf791caf60e60ba96f7b5d6c68793ee7d4c7
tom <tom.guibard45@gmail.com>
2019-01-16 11:38:02 +0100

    Oui

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,19 @@
  */
 
 // any CSS you require will output into a single css file (app.scss in this case)
-require('../css/app.scss');
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 var $ = require('jquery');
-require('bootstrap');
+
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+        return false;
+    });
+});
+
+$('#scrollTop').on('click', function () {
+    $('html, body').animate( { scrollTop: $('#top').offset().top }, 750 );
+    return false;
+});
